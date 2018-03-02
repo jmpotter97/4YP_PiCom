0eab14118b38231ee7d5cccf7a8bef6a87f27d8a
CamEadie <cam.eadie@gmail.com>
2018-03-02 09:36:56 +0000

    Changes to receiver

--- 4YP_PiCom_Receiver/PiComRx_4_ErrorCorrect.py
+++ 4YP_PiCom_Receiver/PiComRx_4_ErrorCorrect.py
@@ -1,12 +1,14 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+from RPiSim.GPIO import GPIO
 
 clock_pin = 18
 data_pin = 4
 output = []
 
-LOGS = "********** RECEIVER LOG FILE **********\n\n"
+LOGS = ["********** RECEIVER LOG FILE **********\n\n"]
 
-def Receive_Data(output):
+
+def Receive_Data(output, LOGS):
     # Data signal should start 1 second after clock to allow for callback setup
     # Wait 5 seconds for the data channel to start transmitting, or timeout
     still_receiving = True
@@ -19,12 +21,13 @@ def Receive_Data(output):
             else:
                 still_receiving = False
     else:
-        LOGS = LOGS + "Data interrput not received, stopping receiver\n"
+        LOGS.append("Data interrput not received, stopping receiver\n")
 
-def Remove_Padding(data_list):
+
+def Remove_Padding(data_list, LOGS):
     # Remove trailing (timeout) zeros
     Strip_Extra(data_list, 0, False)
-    LOGS = LOGS + "SIZE OF INPUT = {}\n".format(len(data_list))
+    LOGS.append("SIZE OF INPUT = {}\n".format(len(data_list)))
     
     # Remove external padding from data
     #   11111110 ... 01111111
@@ -32,11 +35,11 @@ def Remove_Padding(data_list):
     Strip_Extra(data_list, 1, False) # Remove trailing padding '1s
     # Remove extra front and rear 0
     if data_list[0] == 0 and data_list[-1] == 0:
-        LOGS = LOGS + "Input fits padding form at least\n"
+        LOGS.append("Input fits padding form at least\n")
         del data_list[0]
         data_list.pop()
     else:
-        LOGS = LOGS + "Input may have some issues\n"
+        LOGS.append("Input may have some issues\n")
     
     # Remove internal continuous value padding
     i = 5
@@ -49,57 +52,63 @@ def Remove_Padding(data_list):
             data_list.remove(i)
             i = i + 4
         i = i + 1
-    LOGS = LOGS + "SIZE OF OUTPUT = {}\n".format(len(data_list))
+    LOGS.append("SIZE OF OUTPUT = {}\n".format(len(data_list)))
+
 
 def Strip_Extra(list_to_strip, value_to_strip, from_front_T_or_rear_F):
-    if front_T_or_F == True:
+    if from_front_T_or_rear_F:
         while list_to_strip[0] == value_to_strip:
             del list_to_strip[0]
-    elif front_T_or_F == False:
+    else:
         while list_to_strip[-1] == value_to_strip:
             list_to_strip.pop()
+
 
 def Decode_Error_Correction(output):
     ''' TO BE ADDED '''
 
+
 def Save_As_Image(output):
     ''' TO BE ADDED '''
 
-try:
-    GPIO.setmode(GPIO.BCM)
-    # Setup input and clock pins as IN-puts with pull-down (PUD_DOWN) resistor
-    GPIO.setup(data_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(clock_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-    # Waiting for clock start interrupt
-    #   wait_for_edge will return 'None' on timeout, otherwise it returns
-    #   the pin number of clock_pin when the edge is detected
-    if GPIO.wait_for_edge(clock_pin, GPIO.RISING, timeout=10000) is not None:
-        LOGS = LOGS + "Clock interrupt successful, receiver was started\n"
-        Receive_Data(output)
-        if len(output) == 0:
-            LOGS = LOGS + "No data was received\n"
+if __name__ == '__main__':
+    try:
+        GPIO.setmode(GPIO.BCM)
+        # Setup input and clock pins as IN-puts with pull-down (PUD_DOWN) resistor
+        GPIO.setup(data_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(clock_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+        # Waiting for clock start interrupt
+        #   wait_for_edge will return 'None' on timeout, otherwise it returns
+        #   the pin number of clock_pin when the edge is detected
+        if GPIO.wait_for_edge(clock_pin, GPIO.RISING, timeout=10000) is not None:
+            LOGS.append("Clock interrupt successful, receiver was started\n")
+            Receive_Data(output, LOGS)
+            if len(output) == 0:
+                LOGS.append("No data was received\n")
+            else:
+                '''
+                Remove_Padding(output, LOGS) ### WILL INCLUDE ONCE CONFIRMED TO WORK, MAYBE
+                Decode_Error_Correction(output) ### REPLACES PADDING REALLY
+                '''
+                LOGS.append("Saving output to file\n")
+                with open('OUTPUT.txt', 'w') as f:
+                    f.write("".join(str(i) for i in output))
+                '''
+                Save_As_Image(output) ### TO DO WHEN POSSIBLE, REMOVE PREVIOUS SAVE TO FILE
+                '''
         else:
-            '''
-            Remove_Padding(output) ### WILL INCLUDE ONCE CONFIRMED TO WORK, MAYBE
-            Decode_Error_Correction(output) ### REPLACES PADDING REALLY
-            '''
-            LOGS = LOGS + "Saving output to file\n"
-            with open('OUTPUT.txt','w') as f:
-                f.write("".join(str(i) for i in output))
-            '''
-            Save_As_Image(output) ### TO DO WHEN POSSIBLE, REMOVE PREVIOUS SAVE TO FILE
-            '''
-    else:
-        LOGS = LOGS + "Receiver timeout waiting for signal\n"
-        
-            
-except KeyboardInterrupt:
-    LOGS = LOGS + "\nExiting on keyboard interrupt!\n"
-except Exception as e:
-    LOGS = LOGS + "\nExiting on unexpected error!\nError is: {}".format(e)
+            LOGS.append("Receiver timeout waiting for signal\n")
 
-finally:
-    with open('LOGS.txt','w') as f:
-        f.write(LOGS)
-    GPIO.cleanup()
+
+    except KeyboardInterrupt:
+        LOGS.append("\nExiting on keyboard interrupt!\n")
+    except Exception as e:
+        LOGS.append("\nExiting on unexpected error!\nError is: {}".format(e))
+
+    finally:
+        with open('LOGS.txt', 'w') as f:
+            for l in LOGS:
+                f.write(l)
+        GPIO.cleanup()
