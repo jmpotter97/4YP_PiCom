3a16c313c18fc0120333bb4a0876496e8006cea8
CamEadie <cam.eadie@gmail.com>
2018-03-09 00:47:23 +0000

    finishing TimeTest

--- 4YP_PiCom_Transmitter/Part_Tests/PiCom_TimeTest.c
+++ 4YP_PiCom_Transmitter/Part_Tests/PiCom_TimeTest.c
@@ -7,8 +7,14 @@ const uint clock_pin = 4;
 
 int main(int argc, char *argv[]) {
 	if (gpioInitialise()<0) { printf("GPIO INIT FAIL\n"); return 1;}
-	while(int i=0;i<1000;i++) {
-        
+    int mask = (1<<27)|(1<<5)|(1<<6)|(1<<12);
+    int t0=gpioTick();
+	for(int i=0; i<1000; i++) {
+        gpioWrite_Bits_0_31_Clear(mask);
+        gpioWrite_Bits_0_31_Set(mask);
     }
+    int t1 = gpioTick()-t0;
+    printf("Time per transition: %d\nFrequency: %d\n",t1/2000,2000/t1);
+
 	gpioTerminate();
 }
